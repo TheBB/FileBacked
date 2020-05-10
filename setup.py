1037a2d88d40baa17715a2b231bf2ea4a967c4cc
Eivind Fonn <evfonn@gmail.com>
2020-05-10 16:33:34 +0200

    Bump version: 1.1.1 → 2.0.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name='FileBacked',
-    version='1.1.1',
+    version='2.0.0',
     description='Simple file-backed HDF5 storage for Python objects',
     long_description=readme,
     maintainer='Eivind Fonn',
